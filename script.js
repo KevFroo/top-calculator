8e5c08fc10a3f9aa62ba6641af1e20b35b9bdf02
KevFroo <ismrfrog@gmail.com>
2026-01-26 14:54:26 -0600

    Wrote the script for calculations

---
+++ script.js
@@ -0,0 +1,19 @@
+// return the sum of two numbers
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+// return the difference
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+// return the product of two numbers
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+
+// return the fraction of two numbers
+function divide(n1, n2) {
+    return n1 / n2;
+}
