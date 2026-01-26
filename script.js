8cc9544aef1484ac4b3acdddaac69e952fef13a7
KevFroo <ismrfrog@gmail.com>
2026-01-26 16:51:17 -0600

    Content display work

--- script.js
+++ script.js
@@ -18,11 +18,6 @@ function divide(n1, n2) {
     return Math.round((n1 / n2) * 1000) / 1000;
 }
 
-// declare variables
-let n1;
-let operator;
-let n2;
-
 // operate calculations depending on operator
 function operate(n1, n2, operator) {
     switch (operator) {
@@ -41,4 +36,61 @@ function operate(n1, n2, operator) {
         default:
             return;
     }
+}
+
+// declare variables
+let n1;
+let operator;
+let n2;
+
+// html elements
+const calculationHeader = document.querySelector('.calculation');
+
+const numBtn = document.querySelectorAll('.num');
+const operatorBtn = document.querySelectorAll('.op')
+
+// update variables based on availability
+function updateN(arg) {
+    if (operator === undefined || n1 === undefined) {
+        if (n1 === undefined) {
+            n1 = arg;
+        } else {
+            n1 += arg;
+        }
+    } else {
+        if (n2 === undefined) {
+            calculationHeader.innerHTML = '';
+            n2 = arg;
+        } else {
+            n2 += arg;
+        }
+    }
+    updateContent(arg);
+}
+
+// update content in calculation header
+function updateContent(arg) {
+    calculationHeader.innerHTML += arg.toString();
+}
+
+// reset var to undefined
+function reset() {
+    n1 = undefined;
+    operator = undefined;
+    n2 = undefined;
+}
+
+// clear all var and calculation header
+function clear() {
+    calculationHeader.innerHTML = '';
+    reset();
+}
+
+// add DOM event
+for (let num of numBtn) {
+    num.onclick = () => updateN(num.classList[1]);
+}
+
+for (let btn of operatorBtn) {
+    btn.onclick = () => operator = btn.classList[1];
 }
