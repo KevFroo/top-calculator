59748b0d35e9a4fd5c0d597b27e50c0f4dbca033
KevFroo <ismrfrog@gmail.com>
2026-01-27 20:31:57 -0600

    Fixed negative function

--- script.js
+++ script.js
@@ -23,11 +23,6 @@ function divide(n1, n2) {
     }
 }
 
-// check for decimals
-function hasDecimal(n) {
-    return !Number.isInteger(n);
-}
-
 // declare variables
 let n1;
 let operator;
@@ -42,16 +37,23 @@ const clearBtn = document.querySelector('.clear');
 const enterBtn = document.querySelector('.enter');
 
 // update variables based on availability
-function updateN(arg, special) {
-    if (((isAns && operator === undefined) || calculationHeader.innerHTML === 'ERROR') && !special) {
+function updateN(arg) {
+    const isSpecial = arg.includes('-');
+    content = arg;
+
+    if (((isAns && operator === undefined) || calculationHeader.innerHTML === 'ERROR')) {
         clear();
         n1 = arg;
-    } else if (operator === undefined || n1 === undefined) {
+    } else if (operator === undefined) {
         if (n1 === undefined) {
             n1 = arg;
         } else {
-            if (special) {
-                n1 = arg + n1;
+            if (isSpecial && !n1.includes('-')) {
+                n1 = '-' + n1;
+                content = n1;
+            } else if (isSpecial && n1.includes('-')) {
+                n1 = n1.slice(1);
+                content = n1;
             } else {
                 n1 += arg;
             }
@@ -61,37 +63,42 @@ function updateN(arg, special) {
             calculationHeader.innerHTML = '';
             n2 = arg;
         } else {
-            if (special) {
-                n2 = arg + n2;
+            if (isSpecial && !n2.includes('-')) {
+                n2 = '-' + n2;
+                content = n2;
+            } else if (isSpecial && n2.includes('-')) {
+                n2 = n2.slice(1);
+                content = n2;
             } else {
                 n2 += arg;
             }
         }
     }
     isAns = false;
-    updateContent(arg, false, special);
+    updateContent(content, isSpecial);
 }
 
 function updateOperator(op) {
     if ((operator !== undefined && n2 !== undefined) && calculationHeader.innerHTML !== 'ERROR') {
         operate(parseFloat(n1), parseFloat(n2), operator);
         operator = op;
-    } else {
+    } else if (n1 !== undefined) {
         operator = op;
     }
 }
 
 // update content in calculation header
-function updateContent(arg, reset, special) {
+function updateContent(arg, reset) {
+    const isSpecial = arg.includes('-');
     if (reset) {
         calculationHeader.innerHTML = arg.toString();
-    } else if (special) {
+    } else if (isSpecial) {
         calculationHeader.innerHTML = arg.toString() + calculationHeader.innerHTML;
     } else {
         calculationHeader.innerHTML += arg.toString();
     }
 
-    if (hasDecimal(parseFloat(calculationHeader.innerHTML))) {
+    if (calculationHeader.innerHTML.includes('.')) {
         let valueSplit = calculationHeader.innerHTML;
         valueSplit = valueSplit.split('.');
         if (valueSplit[1].length > 6) {
@@ -103,13 +110,11 @@ function updateContent(arg, reset, special) {
 // handles special conditions with '-' and '.'
 function specialN(arg) {
     if (arg === '.') {
-        if (!hasDecimal(parseFloat(calculationHeader.innerHTML))) {
-            updateN(arg, false);
+        if (!calculationHeader.innerHTML.includes('.')) {
+            updateN(arg);
         }
     } else {
-        if (parseFloat(calculationHeader.innerHTML) > 0) {
-            updateN(arg, true);
-        }
+        updateN(arg);
     }
 }
 
@@ -167,7 +172,7 @@ for (let btn of numBtn) {
     if (btn.classList[1] === 'special') {
         btn.onclick = () => specialN(btn.classList[2]);
     } else {
-        btn.onclick = () => updateN(btn.classList[1], false);
+        btn.onclick = () => updateN(btn.classList[1]);
     }
 }
 for (let btn of operatorBtn) {
